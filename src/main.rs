f35e3df56d9a4a1af7c00ebd26d62ef7372c9fca
anatawa12 <anatawa12@icloud.com>
2023-10-04 12:52:10 +0900

    reimplementation

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ use std::fs::File;
 use std::io;
 use std::io::Write;
 use std::marker::PhantomData;
+use std::sync::Arc;
 use v4l::prelude::*;
 use v4l::{Format, FourCC, Memory};
 use v4l::buffer::{Metadata, Type};
 use v4l::capability::Flags;
+use v4l::device::{Handle, MultiPlaneDevice};
+use v4l::format::MultiPlaneFormat;
+use v4l::io::mmap::stream;
 use v4l::io::Queue;
 use v4l::io::queue::Streaming;
+use v4l::io::traits::{CaptureStream, OutputStream, Stream};
 use v4l::memory::Mmap;
 use v4l::video::{Capture, capture, Output, output};
 
@@ -16,24 +21,14 @@ fn main() -> io::Result<()> {
     let camera_device = 0;
     let encoder_device = 11;
     let fps = 10;
+    let width = 640;
+    let height = 480;
+    let camera_fourcc = FourCC::new(b"RGB3");
+    let encoded_fourcc = FourCC::new(b"H264");
 
-    let camera_format = Format {
-        width: 640,
-        height: 480,
-        fourcc: FourCC::new(b"RGB3"),
-        ..unsafe { std::mem::zeroed() }
-    };
+    //let mut camera = Device::new(camera_device).unwrap();
 
-    let encoded_format = Format {
-        width: 640,
-        height: 480,
-        fourcc: FourCC::new(b"H264"),
-        ..unsafe { std::mem::zeroed() }
-    };
-
-    //let mut camera = Device::new(camera_device)?;
-
-    //let camera_caps = camera.query_caps()?;
+    //let camera_caps = camera.query_caps().unwrap();
     //if !camera_caps.capabilities.contains(Flags::VIDEO_CAPTURE) {
     //    panic!("Camera: Capture not supported")
     //}
@@ -41,104 +36,149 @@ fn main() -> io::Result<()> {
     //    panic!("Camera: Streaming not supported")
     //}
 
-    //Capture::set_format(&mut camera, &camera_format)?;
-    //Capture::set_params(&mut camera, &capture::Parameters::with_fps(fps))?;
+    //Capture::set_format(&mut camera, &camera_format).unwrap();
+    //Capture::set_params(&mut camera, &capture::Parameters::with_fps(fps)).unwrap();
 
-    let mut encoder = Device::new(encoder_device)?;
+    let mut encoder = MultiPlaneDevice::new(encoder_device).unwrap();
 
-    let encoder_caps = encoder.query_caps()?;
+    let encoder_caps = encoder.query_caps().unwrap();
     println!("Encoder capabilities: {}", encoder_caps.capabilities);
-    if !encoder_caps.capabilities.contains(Flags::VIDEO_CAPTURE) {
+    if !encoder_caps.capabilities.contains(Flags::VIDEO_M2M_MPLANE) {
         panic!("Encoder: Capture not supported")
-    }
-    if !encoder_caps.capabilities.contains(Flags::VIDEO_OUTPUT) {
-        panic!("Encoder: Output not supported")
     }
     if !encoder_caps.capabilities.contains(Flags::STREAMING) {
         panic!("Encoder: Streaming not supported")
     }
 
-    Output::set_format(&mut encoder, &camera_format)?;
-    Capture::set_format(&mut encoder, &encoded_format)?;
-    Output::set_params(&mut encoder, &output::Parameters::with_fps(fps))?;
+    Output::set_format(&mut encoder, &MultiPlaneFormat::single_plane(width, height, camera_fourcc)).unwrap();
+    Capture::set_format(&mut encoder, &MultiPlaneFormat::single_plane(width, height, encoded_fourcc)).unwrap();
+    Output::set_params(&mut encoder, &output::Parameters::with_fps(fps)).unwrap();
 
-    //let mut camera_stream = CaptureStream::with_device(&camera, 3)?;
-    let mut encoder_raw_stream = OutputStream::with_device(&encoder, 1)?;
-    let mut encoder_encoded_queue = CaptureStream::with_device(&encoder, 1)?;
+    //let mut camera_stream = CaptureStream::with_device(&camera, 3).unwrap();
+    let mut encoder_raw_stream1 = MmapStream::with_buffers(&encoder, Type::VideoOutputMplane, 1).unwrap();
+    let mut encoder_encoded_stream1 = MmapStream::with_buffers(&encoder, Type::VideoCaptureMplane, 1).unwrap();
+    //let mut encoder_raw_stream = MultiPlaneOutputStream::with_device(&encoder, 1).unwrap();
+    //let mut encoder_encoded_queue = MultiPlaneCaptureStream::with_device(&encoder, 1).unwrap();
 
-    let mut write_to = File::open("test.h264")?;
+    OutputStream::queue(&mut encoder_raw_stream1, 0).unwrap();
+    CaptureStream::queue(&mut encoder_encoded_stream1, 0).unwrap();
+
+    encoder_raw_stream1.start().unwrap();
+    encoder_encoded_stream1.start().unwrap();
+
+    let mut write_to = File::create("test.h264").unwrap();
 
     for i in 0..100 {
-        encoder_raw_stream.write_frame(|buffer| {
-            //camera_stream.read_frame(|frame| {
-            //    buffer.copy_from_slice(frame);
-            //    Ok(frame.len())
-            //})
-            buffer[..640 * 480 * 3].fill(i);
-            Ok(640 * 480 * 3)
-        })?;
+        println!("frame {i}");
+        let index = OutputStream::dequeue(&mut encoder_raw_stream1).unwrap();
+        println!("frame {i}: deq");
+        let (out_buffers, _meta, planes) = OutputStream::get(&mut encoder_raw_stream1, index).unwrap();
+        out_buffers[0][..640 * 480 * 3].fill(i);
+        planes[0].bytesused = 640 * 480 * 3;
+        println!("frame {i}: queueing");
+        OutputStream::queue(&mut encoder_raw_stream1, index).unwrap();
+        println!("frame {i}: que");
 
-        encoder_encoded_queue.read_frame(|buffer| {
-            write_to.write(buffer)?;
-            Ok(())
-        })?;
+        let index = CaptureStream::dequeue(&mut encoder_encoded_stream1).unwrap();
+        println!("frame {i}: deq");
+        let (out_buffers, _meta, planes) = CaptureStream::get(&encoder_encoded_stream1, index).unwrap();
+        write_to.write(&out_buffers[0][..planes[0].bytesused as usize]).unwrap();
+        OutputStream::queue(&mut encoder_encoded_stream1, index).unwrap();
+        println!("frame {i}: que");
+
+        //encoder_raw_stream.write_frame(|buffer| {
+        //    //camera_stream.read_frame(|frame| {
+        //    //    buffer.copy_from_slice(frame);
+        //    //    Ok(frame.len())
+        //    //})
+        //    buffer[..640 * 480 * 3].fill(i);
+        //    Ok(640 * 480 * 3)
+        //}).unwrap();
+
+        //encoder_encoded_queue.read_frame(|buffer| {
+        //    write_to.write(buffer).unwrap();
+        //    Ok(())
+        //}).unwrap();
     }
 
-    //camera_stream.finish()?;
-    encoder_raw_stream.finish()?;
-    encoder_encoded_queue.finish()?;
+    //camera_stream.finish().unwrap();
+    //encoder_raw_stream.finish().unwrap();
+    //encoder_encoded_queue.finish().unwrap();
 
     Ok(())
 }
 
 fn buffer_metadata(index: usize) -> Metadata {
     Metadata {
-        memory: Memory::Mmap,
         index: index as u32,
-        ..unsafe { std::mem::zeroed() }
+        length: 1,
+        ..Metadata::with_memory(Memory::Mmap)
     }
 }
 
-type CaptureStream<'a> = StreamBase<'a, VideoCapture>;
-type OutputStream<'a> = StreamBase<'a, VideoOutput>;
+//type CaptureStream<'a> = StreamBase<'a, VideoCapture>;
+//type OutputStream<'a> = StreamBase<'a, VideoOutput>;
+#[allow(dead_code)]
+type MultiPlaneCaptureStream<'a> = StreamBase<'a, MultiPlaneVideoCapture>;
+#[allow(dead_code)]
+type MultiPlaneOutputStream<'a> = StreamBase<'a, MultiPlaneVideoOutput>;
 
 trait StreamTypeMarker {
     const TYPE: Type;
 }
 
-struct VideoCapture;
-
-impl StreamTypeMarker for VideoCapture {
-    const TYPE: Type = Type::VideoCapture;
+macro_rules! marker {
+    ($name: ident, $value: ident) => {
+        struct $name;
+        impl StreamTypeMarker for $name {
+            const TYPE: Type = Type::$value;
+        }
+    };
 }
 
-struct VideoOutput;
-
-impl StreamTypeMarker for VideoOutput {
-    const TYPE: Type = Type::VideoCapture;
-}
+marker!(VideoCapture, VideoCapture);
+marker!(VideoOutput, VideoOutput);
+marker!(MultiPlaneVideoCapture, VideoCaptureMplane);
+marker!(MultiPlaneVideoOutput, VideoOutputMplane);
 
 struct StreamBase<'a, Type: StreamTypeMarker> {
     queue: Queue<Mmap<'a>, Streaming>,
     _phantom: PhantomData<Type>,
 }
 
+macro_rules! with_device {
+    ($Device: ident, $Type: ty) => {
+        impl <'a> StreamBase<'a, $Type> {
+            #[inline]
+            #[allow(dead_code)]
+            pub fn with_device(device: &$Device, buf_count: u32) -> io::Result<Self> {
+                Self::with_device_impl(device.handle(), buf_count)
+            }
+        }
+    };
+}
+
+with_device!(MultiPlaneDevice, MultiPlaneVideoCapture);
+with_device!(MultiPlaneDevice, MultiPlaneVideoOutput);
+with_device!(Device, VideoCapture);
+with_device!(Device, VideoOutput);
+
 impl <'a, Type : StreamTypeMarker> StreamBase<'a, Type> {
-    pub fn with_device(device: &Device, buf_count: u32) -> io::Result<Self> {
-        let mut queue = Queue::with_mmap(device.handle(), Type::TYPE, buf_count)?;
+    fn with_device_impl(device: Arc<Handle>, buf_count: u32) -> io::Result<Self> {
+        let mut queue = Queue::with_mmap(device, Type::TYPE, buf_count).unwrap();
 
         for i in 0..queue.len() {
-            queue.enqueue(&buffer_metadata(i))?;
+            queue.enqueue(&buffer_metadata(i)).unwrap();
         }
 
-        let queue = queue.start_stream()?;
+        let queue = queue.start_stream().unwrap();
 
         return Ok(Self { queue, _phantom: PhantomData })
     }
-
+    
     pub fn finish(self) -> io::Result<()> {
         let Self{ queue, _phantom } = self;
-        queue.stop_stream()?;
+        queue.stop_stream().unwrap();
         Ok(())
     }
 }
@@ -148,10 +188,10 @@ impl <'a> StreamBase<'a, VideoCapture> {
         &mut self,
         process_data: impl for <'b> FnOnce(&'b [u8]) -> io::Result<R>,
     ) -> io::Result<R> {
-        let encoder_encoded_meta = self.queue.dequeue()?;
+        let encoder_encoded_meta = self.queue.dequeue().unwrap();
         let encoder_encoded_buf = &self.queue[encoder_encoded_meta.index as usize].0[..encoder_encoded_meta.bytesused as usize];
-        let result = process_data(encoder_encoded_buf)?;
-        self.queue.enqueue(&encoder_encoded_meta)?;
+        let result = process_data(encoder_encoded_buf).unwrap();
+        self.queue.enqueue(&encoder_encoded_meta).unwrap();
         Ok(result)
     }
 }
@@ -161,10 +201,10 @@ impl <'a> StreamBase<'a, VideoOutput> {
         &mut self,
         get_raw_data: impl for <'b> FnOnce(&'b mut [u8]) -> io::Result<usize>,
     ) -> io::Result<()> {
-        let mut encoder_encoded_meta = self.queue.dequeue()?;
+        let mut encoder_encoded_meta = self.queue.dequeue().unwrap();
         let encoder_encoded_buf = &mut self.queue[encoder_encoded_meta.index as usize].0[..];
-        encoder_encoded_meta.bytesused = get_raw_data(encoder_encoded_buf)? as u32;
-        self.queue.enqueue(&encoder_encoded_meta)?;
+        encoder_encoded_meta.bytesused = get_raw_data(encoder_encoded_buf).unwrap() as u32;
+        self.queue.enqueue(&encoder_encoded_meta).unwrap();
         Ok(())
     }
 }
